910375c02a4562c3314c3bde46e6e43327155236
TeClosure <tys.jill@gmail.com>
2024-01-19 17:31:27 +0900

    User registration API added

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -1,9 +1,38 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+    "admin/src/database"
+    "admin/src/models"
+
+    "github.com/gofiber/fiber/v2"
+    "golang.org/x/crypto/bcrypt"
+)
 
 func Register(ctx *fiber.Ctx) error {
-	return ctx.JSON(fiber.Map{
-		"message": "hello",
-	})
+    var data map[string]string
+
+    if err:= ctx.BodyParser(&data); err != nil {
+        return err
+    }
+
+    if data["password"] != data["password_confirm"] {
+        ctx.Status(fiber.StatusBadRequest) // 400
+        return ctx.JSON(fiber.Map{
+            "message": "Password is incorrect.",
+        })
+    }
+
+    pwd, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 12)
+
+    user := models.User{
+        FirstName:      data["first_name"],
+        LastName:       data["last_name"],
+        Email:          data["email"],
+        Password:       pwd,
+        IsAmbassdor:    false,
+    }
+
+    database.DB.Create(&user)
+
+    return ctx.JSON(user)
 }
